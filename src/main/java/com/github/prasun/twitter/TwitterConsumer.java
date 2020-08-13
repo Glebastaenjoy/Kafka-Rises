808be00f6d757c8c6e847c8ebd196ab1a8e81308
Prasun Chakraborty <lordprasun@gmail.com>
2020-08-13 19:21:33 +0530

    Making the twitter consumer Idempotent

--- src/main/java/com/github/prasun/twitter/TwitterConsumer.java
+++ src/main/java/com/github/prasun/twitter/TwitterConsumer.java
@@ -1,7 +1,10 @@
 package com.github.prasun.twitter;
 
 import com.github.prasun.kafkamain.consumerDemo;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mongodb.DBObject;
+import com.mongodb.MongoWriteException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -17,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Properties;
 
 public class TwitterConsumer {
@@ -24,36 +28,57 @@ public class TwitterConsumer {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(consumerDemo.class);
         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        MongoDatabase db = mongoClient.getDatabase("kafkaDump");
-        MongoCollection<Document> tweetsCollection = db.getCollection("tweets2");
+        MongoDatabase db = mongoClient.getDatabase("tweets");
+        MongoCollection<Document> tweetsCollection = db.getCollection("test");
         KafkaConsumer<String,String> consumer = createConsumer("tweets");
-
+        int count=0;
         while(true){
             ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String,String> record: records){
+                count++;
                 logger.info("Key:"+record.key()+" Value:"+record.value());
                 logger.info("Partition:"+record.partition()+", Offset:"+record.offset());
-                Document tweet = Document.parse(record.value());
-                tweetsCollection.insertOne(tweet);
-                System.out.println("DONE");
-            }
+                // Unique id for Idempotent Consumer
+                // Kafka Generic Id -> record.topic()+"_"+record.partition()+"_"+record.offset();
+                // Stream specific id:
+                HashMap<String,String> tweetRecs = extractIdFromTweet(record.value());
+                Document tweet = new Document("_id",tweetRecs.get("id_str"));
+                tweet.append("data",Document.parse(record.value()));
+                tweet.append("name",tweetRecs.get("name"));
+                tweet.append("text",tweetRecs.get("text"));
+                try {
+                    tweetsCollection.insertOne(tweet);
+                }catch (MongoWriteException e){
+                    e.printStackTrace();
+                }
 
+            }
         }
 
     }
 
     public static KafkaConsumer<String,String> createConsumer(String topic){
         String bootstrapServer = "127.0.0.1:9092";
-        String groupId = "tweets_groups";
+        String groupId = "tweets_group";
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
 
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
         consumer.subscribe(Collections.singleton(topic));
         return consumer;
+    }
+    private static final JsonParser jsonParser = new JsonParser();
+    private static HashMap<String, String>  extractIdFromTweet(String tweetJson){
+        // Using Gson library
+        JsonObject jsonObject=  jsonParser.parse(tweetJson).getAsJsonObject();
+        HashMap<String,String> tweetIds = new HashMap<>();
+        tweetIds.put("id_str",jsonObject.get("id_str").getAsString());
+        tweetIds.put("text",jsonObject.get("text").getAsString());
+        tweetIds.put("name",jsonObject.getAsJsonObject("user").get("name").getAsString());
+        return tweetIds;
     }
 }
