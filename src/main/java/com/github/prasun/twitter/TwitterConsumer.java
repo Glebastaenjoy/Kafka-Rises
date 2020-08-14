579c7cf2ceff8bed65c0df61cb0ea71dfc748bf6
Prasun Chakraborty <lordprasun@gmail.com>
2020-08-14 09:45:39 +0530

    Offset commit manually

--- src/main/java/com/github/prasun/twitter/TwitterConsumer.java
+++ src/main/java/com/github/prasun/twitter/TwitterConsumer.java
@@ -29,13 +29,12 @@ public class TwitterConsumer {
         Logger logger = LoggerFactory.getLogger(consumerDemo.class);
         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
         MongoDatabase db = mongoClient.getDatabase("tweets");
-        MongoCollection<Document> tweetsCollection = db.getCollection("test");
+        MongoCollection<Document> tweetsCollection = db.getCollection("rhea");
         KafkaConsumer<String,String> consumer = createConsumer("tweets");
-        int count=0;
         while(true){
             ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(100));
+            logger.info("Received:"+records.count()+ " records");
             for (ConsumerRecord<String,String> record: records){
-                count++;
                 logger.info("Key:"+record.key()+" Value:"+record.value());
                 logger.info("Partition:"+record.partition()+", Offset:"+record.offset());
                 // Unique id for Idempotent Consumer
@@ -53,20 +52,23 @@ public class TwitterConsumer {
                 }
 
             }
+            consumer.commitSync();
+            logger.info("Offsets Commited");
         }
 
     }
 
     public static KafkaConsumer<String,String> createConsumer(String topic){
         String bootstrapServer = "127.0.0.1:9092";
-        String groupId = "tweets_group";
+        String groupId = "tweets_groups";
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
-
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"20");
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
         consumer.subscribe(Collections.singleton(topic));
         return consumer;
