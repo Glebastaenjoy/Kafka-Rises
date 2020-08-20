5d167ff6cd69aea3cfa788cc87c2513eb65d8ac0
Prasun Chakraborty <lordprasun@gmail.com>
2020-08-20 11:37:25 +0530

    Adding Consumer

--- kafka-producer.py
+++ kafka-producer.py
@@ -5,15 +5,23 @@ import time
 
 
 def json_serializer(data):
+    """ Serialize the data sending via network"""
     return json.dumps(data).encode('utf-8')
 
 
+def get_partition(key_bytes, all_partition, available_partition):
+    """ Select the partition in which to send the data """
+    return available_partition[0]
+
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=json_serializer)
+                         value_serializer=json_serializer,
+                         # partitioner=get_partition
+                         )
 
 if __name__ == "__main__":
     count = 30
     while count > 0:
         count -= 1
         producer.send('myfirst_topic', get_registered_user())
-        time.sleep(2)
+        time.sleep(1)
